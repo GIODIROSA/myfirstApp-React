da7e329c08cbeef0dbc09b8ef0af73417ec2833b
Giovanni Di Rosa <giodirosaartes@gmail.com>
2021-05-17 23:33:49 -0400

    tipos de function y syntax ademas props uso

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//function convencional
+
+function HolaMundo(props) {
+  return <div id="mundo">{props.mytext}</div>;
 }
+
+//otra manera de desarrollar la function-tercera opción
+
+class Loquesea extends React.Component {
+  render() {
+    return <div>Esto es loquesea que se esta escribiendo.</div>;
+  }
+}
+
+//estructurar una function con arrow function
+
+const Valentina = (props) => <div id="peinado">{props.secorto}</div>;
+
+const App = () => (
+  <div>
+    This is my Components: <HolaMundo mytext="Hola Mundo"/>
+    <Valentina secorto="Valentina se corto el cabello" /> <Loquesea />
+  </div>
+);
 
 export default App;
